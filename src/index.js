a3ec20746cb2adcf25857d56a059bf56c8d9e328
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2025-09-03 08:35:50 +0300

    add HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -11,9 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/booking-frontend">
-        <App />
-      </BrowserRouter>
+      {/* <BrowserRouter basename="/booking-frontend"> */}
+        <Router>
+          <App />
+        </Router>
+      {/* </BrowserRouter> */}
     </Provider>
   </React.StrictMode>
 );
